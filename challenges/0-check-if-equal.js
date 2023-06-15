fa581b340f840a2090b2ee15f3f74113d85cb974
TomOwen93 <mr.tsowen@gmail.com>
2023-06-15 13:56:27 +0100

    JS implementation of "check if equal" challenge

---
+++ challenges/0-check-if-equal.js
@@ -0,0 +1,43 @@
+// Write a function that, given two numbers, returns a boolean representing whether or not the two numbers are equal.
+
+// You may assume that the numbers are always integers (i.e. whole numbers).
+
+// function:
+// =========
+// name: isEqual
+
+// parameters:
+// 	- NUM_ONE: number
+// 	- NUM_TWO: number
+
+// return: boolean
+
+// pseudocode:
+// ============
+// if NUM_ONE is equal to NUM_TWO
+// 	return True
+// else
+// 	return False
+
+// test cases:
+// ===========
+// isEqual(4, 4) should be True
+// isEqual(0, 0) should be True
+// isEqual(-4, -4) should be True
+// isEqual(4, -4) should be False
+// isEqual(0, 1) should be False
+
+function isEqual(num1, num2) {
+  if (num1 == num2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+/** Test Cases: */
+console.log("isEqual(4, 4):", isEqual(4, 4), "should be true");
+console.log("isEqual(-4, -4)", isEqual(-4, -4), "should be false");
+console.log("isEqual(4, -4)", isEqual(4, -4), "should be false");
+console.log("isEqual(0, 0)", isEqual(0, 0), "should be true");
+console.log("isEqual(0, 1)", isEqual(0, 1), "should be false");
